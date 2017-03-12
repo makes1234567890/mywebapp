f15f868ba36a56ce82e134aa5f6d992dfac42375
matak <matak@LAVIE-Q646OUH>
2017-03-12 17:28:19 +0900

    クラス名修正

---
+++ ContentsDistributionSystem/src/system/distribution/contents/Constants.java
@@ -0,0 +1,9 @@
+package system.distribution.contents;
+/**
+ * 定数
+ * @author matak
+ *
+ */
+public class Constants {
+	public static final String PARAMETER_MAPFILENAMEDLPATH = "mapFilenameDLPath";
+}
--- ContentsDistributionSystem/src/system/distribution/contents/Contstants.java
+++
@@ -1,8 +0,0 @@
-package system.distribution.contents;
-/**
- * 定数
- * @author matak
- *
- */
-public class Contstants {
-}
